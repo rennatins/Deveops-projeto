a97bc466f741a53e8a1be2f92eac822559670f53
rennan felipe <rennanfelipe2003@gmail.com>
2026-04-18 17:54:22 -0300

    Projeto inicial com Docker

---
+++ main.py
@@ -0,0 +1,15 @@
+def calcular(a, b, operacao):
+    if operacao == "1":
+        return a + b
+    elif operacao == "2":
+        return a - b
+    elif operacao == "3":
+        return a * b
+    elif operacao == "4":
+        return a / b
+    else:
+        return None
+
+
+if __name__ == "__main__":
+    print("Aplicação rodando com sucesso!")
