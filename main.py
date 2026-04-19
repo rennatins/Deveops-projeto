8bd966d69a3a7cd8ef910d85a04fcbf3397b2eb7
rennan felipe <rennanfelipe2003@gmail.com>
2026-04-18 22:47:01 -0300

    Alteração 3

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 def calcular(a, b, operacao):
     if operacao == "1":
         return a + b
@@ -7,11 +6,41 @@ def calcular(a, b, operacao):
     elif operacao == "3":
         return a * b
     elif operacao == "4":
+        if b == 0:
+            return "Erro: divisão por zero"
         return a / b
     else:
         return None
 
 
+def main():
+    print("Calculadora Simples em Python")
+    print("-------------------------------")
+
+    try:
+        a = float(input("Digite o primeiro número: "))
+        b = float(input("Digite o segundo número: "))
+
+        print("\nEscolha a operação:")
+        print("1 - Soma")
+        print("2 - Subtração")
+        print("3 - Multiplicação")
+        print("4 - Divisão")
+
+        op = input("Opção: ")
+
+        resultado = calcular(a, b, op)
+
+        if resultado is None:
+            print("Operação inválida")
+        else:
+            print(f"Resultado: {resultado}")
+
+    except ValueError:
+        print("Erro: digite apenas números válidos")
+
+    print("\n✔ Sistema finalizado.")
+
+
 if __name__ == "__main__":
-    print("Calculadora rodando com sucesso! 🚀")
-    print("Atividade DevOps")
+    main()
